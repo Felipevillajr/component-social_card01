360801ba3dfe4cb4eb966d784ad13566573b6ed5
felipevillajr <felipevilla2010@live.com>
2021-12-10 13:01:56 -0500

    finished styling text below img and added icons for social

--- src/Components/socialcard/socialcard.js
+++ src/Components/socialcard/socialcard.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./socialcard.scss";
+import comment from "../../Images/icons/comments.png";
+import repost from "../../Images/icons/repost.png";
+import liked from "../../Images/icons/heart.png";
+import likeaf from "../../Images/icons/heart_after.png";
+import mail from "../../Images/icons/mail.png";
 
 export default function socialcard() {
   return (
@@ -33,21 +38,34 @@ export default function socialcard() {
       <div className="socialcard__mid">
         <div className="socialcard__mid__textbox">
           <div className="socialcard__mid__img"></div>
-          <div className="socialcard__mid__textbox__posttitle">
-            Learning React? Start Small
+          <div className="socialcard__mid__text">
+            <div className="socialcard__mid__textbox__posttitle">
+              Learning React? Start Small.
+            </div>
+            <div className="socialcard__mid__textbox__postcontent">
+              Can't pry yourself away from the tutorials? The cure is to make
+              tiny little experiment apps.
+            </div>
+            <div className="socialcard__mid__textbox__postlink">dev.to</div>
           </div>
-          <div className="socialcard__mid__textbox__postcontent">
-            Can't pry yourself away from the tutorials? The cure is to make tiny
-            little experiment apps.
-          </div>
-          <div className="socialcard__mid__textbox__postlink">dev.to</div>
         </div>
       </div>
       <div className="socialcard__bot">
-        <div className="socialcard__bot__commenticon"></div>
-        <div className="socialcard__bot__reposticon"></div>
-        <div className="socialcard__bot__likedicon"></div>
-        <div className="socialcard__bot__mailicon"></div>
+        <img
+          className="socialcard__bot__commenticon"
+          src={comment}
+          alt="comment icon"
+        />
+        <div className="socialcard__bot__comment">200</div>
+        <img
+          className="socialcard__bot__reposticon"
+          src={repost}
+          alt="repost"
+        />
+        <div className="socialcard__bot__repost">2</div>
+        <img className="socialcard__bot__likedicon" src={liked} alt="liked" />
+        <div className="socialcard__bot__liked">2235</div>
+        <img className="socialcard__bot__mailicon" src={mail} alt="mail" />
       </div>
     </div>
   );
