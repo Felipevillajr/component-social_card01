3ac6f11232544e3c5ecd9a8bae8d2263c82321ed
felipevillajr <felipevilla2010@live.com>
2021-12-10 13:09:34 -0500

    added disclaimer and link to tutorial

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import Socialcard from "./Components/socialcard/socialcard.js";
 export default function App() {
   return (
     <div className="app">
+      <div className="app__title">
+        <h1>the component below was created for learning purposes from</h1>
+
+        <a
+          className="app__title__link"
+          href="https://daveceddia.com/react-practice-projects/"
+        >
+          here
+        </a>
+      </div>
+
       <Socialcard />
+
+      <h3>not designed for responsiveness</h3>
     </div>
   );
 }
